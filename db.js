83dc31e83fec0988c6f0a2d92e83b1c9cf0f9b44
noir <anoirsmail@gmail.com>
2023-03-31 17:57:04 +0600

    configs done

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
-const Pool = require("pg").Pool;
+require('dotenv').config();
+
+const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: "postgres",
-  password: "root",
-  host: "localhost",
-  port: 5432,
-  database: "careerist_back"
-})
+  connectionString: process.env.DB_URI,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+pool.query('SELECT NOW()').then((result) => {
+  console.log('Connected to database:', result.rows[0].now);
+}).catch((error) => {
+  console.error('Error connecting to database:', error);
+});
 
 module.exports =pool;
